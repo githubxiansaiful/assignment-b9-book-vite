e89766fd5e2e2f689dd5cdc319fc0cef11950806
Xian Saiful <xiansaiful@gmail.com>
2024-03-28 09:59:40 +0600

    Fixed some issue

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,10 @@
 import './App.css'
-import Home from './pages/Home'
-import ListedBooks from './pages/ListedBooks'
-import PagesToRead from './pages/PagesToRead'
 
 function App() {
 
   return (
     <>
-      <Home></Home>
-      <ListedBooks></ListedBooks>
-      <PagesToRead></PagesToRead>
+      
     </>
   )
 }
